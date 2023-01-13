9299a8e8d9943adc8940354fc1c091da08caecd9
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-01-13 10:22:47 -0800

    created screen

---
+++ Breakout/Breakout.py
@@ -0,0 +1,7 @@
+import pygame
+#creating a screen, incomplete 
+pygame.init()
+background = pygame.display.set_mode((1300, 650))
+screen.fill((255, 255, 255))#figure out coloring
+pygame.display.flip()
+pygame.display.set_caption('Breakout')
