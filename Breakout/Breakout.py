8dbc78ad541f8c5b35220df435c9621441e23488
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-01-18 10:10:41 -0800

    cleaned up code

--- Breakout/Breakout.py
+++ Breakout/Breakout.py
@@ -1,27 +1,8 @@
-#import pygame
-#creating a screen, incomplete 
-#background.fill((255, 0, 0))#figure out coloring, DONE
-#pygame.display.flip()
-#pygame.display.set_caption('Breakout')
- 
-
-#running = True
-#while running:
-    #for event in pygame.event.get():
-        #if event.type == pygame.QUIT:
-            #running = False
 import pygame
- 
-# Initializing Pygame
 pygame.init()
- 
-# Initializing surface
+pygame.display.set_caption('Breakout')
 surface = pygame.display.set_mode((1300,650))
- 
-# Initializing Color
 color = (255,0,0)
- 
-# Drawing Rectangle
 pygame.draw.rect(surface, color, pygame.Rect(650, 550, 100, 30),  2)
 pygame.display.flip()
 running = True
