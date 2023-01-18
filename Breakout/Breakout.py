1e096be3dc90bd2f9e351d2cd22ecee9797f9315
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-01-18 10:22:18 -0800

    started circle, incomplete

--- Breakout/Breakout.py
+++ Breakout/Breakout.py
@@ -1,10 +1,16 @@
 import pygame
 pygame.init()
+#screen
 pygame.display.set_caption('Breakout')
 surface = pygame.display.set_mode((1300,650))
-color = (255,0,0)
-pygame.draw.rect(surface, color, pygame.Rect(650, 550, 100, 30),  2)
+#paddle
+RED = (255,0,0)
+pygame.draw.rect(surface, RED, pygame.Rect(650, 550, 100, 30),  2)
 pygame.display.flip()
+#circle
+BLUE = (0, 0, 255)
+pygame.draw.circle#incomplete line START HERE
+#loop preventing finish
 running = True
 while running:
     for event in pygame.event.get():
