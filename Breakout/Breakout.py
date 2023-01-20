edf929c9a93b623c0ff4dba607a5e88978f23b9d
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-01-20 09:01:51 -0800

    added ball, made objects solid

--- Breakout/Breakout.py
+++ Breakout/Breakout.py
@@ -5,11 +5,13 @@ pygame.display.set_caption('Breakout')
 surface = pygame.display.set_mode((1300,650))
 #paddle
 RED = (255,0,0)
-pygame.draw.rect(surface, RED, pygame.Rect(650, 550, 100, 30),  2)
+pygame.draw.rect(surface, RED, pygame.Rect(650, 550, 100, 30))
 pygame.display.flip()
 #circle
 BLUE = (0, 0, 255)
-pygame.draw.circle#incomplete line START HERE
+pygame.draw.circle(surface, (BLUE), [650, 325], 15)
+ # Draws the surface object to the screen.
+pygame.display.update()
 #loop preventing finish
 running = True
 while running:
