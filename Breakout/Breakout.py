f6536d278b242fa9e71cd38c4670a588474fab51
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-01-20 09:37:51 -0800

    sending move error

--- Breakout/Breakout.py
+++ Breakout/Breakout.py
@@ -4,12 +4,21 @@ pygame.init()
 pygame.display.set_caption('Breakout')
 surface = pygame.display.set_mode((1300,650))
 #paddle
+k1 = 650
+k2 = 600
+paddlemove = 5
 RED = (255,0,0)
-pygame.draw.rect(surface, RED, pygame.Rect(650, 550, 100, 30))
+paddle = pygame.draw.rect(surface, RED, pygame.Rect(k1, k2, 100, 30))
 pygame.display.flip()
 #circle
 BLUE = (0, 0, 255)
-pygame.draw.circle(surface, (BLUE), [650, 325], 15)
+ball = pygame.draw.circle(surface, (BLUE), [650, 325], 15)
+#key press to move paddle
+key_input = pygame.key.get_pressed()
+if key_input [pygame.K_LEFT]:
+    paddle.move(-paddlemove, 0)
+if key_input [pygame.K_RIGHT]:
+    paddle.move(paddlemove, 0)
  # Draws the surface object to the screen.
 pygame.display.update()
 #loop preventing finish
