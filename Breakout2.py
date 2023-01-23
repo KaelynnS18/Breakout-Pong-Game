e0592fbe78783befd2e995765ca7b3cf587acd6d
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-01-23 14:24:19 -0800

    paddle and ball working

--- Breakout2.py
+++ Breakout2.py
@@ -2,14 +2,24 @@ import pygame
 pygame.init()
 clock = pygame.time.Clock()
 
+screen_height = 650
+screen_width = 1300
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
-paddle_x = 500
-paddle_y = 300
+paddle_x = 650
+paddle_y = 600
 paddlemove = 5
+ballx = 650
+bally = 325
+ballmove_x = 5
+ballmove_y = 5
+screen_left = 40
+screen_right = screen_width - 40
+screen_top = 40
+screen_bottom = screen_height - 40
 
-screen = pygame.display.set_mode((1300, 650))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Breakout')
 
 running = True
@@ -22,13 +32,36 @@ while running:
     if key_input [pygame.K_LEFT]:
         paddle_x -= paddlemove
     if key_input [pygame.K_RIGHT]:
-        paddle_y += paddlemove
+        paddle_x += paddlemove
 
     screen.fill(BLACK)
 
-    paddle = pygame.draw.rect(screen, RED, (paddle_x, paddle_y, 50, 40))
+    paddle = pygame.draw.rect(screen, RED, pygame.Rect(paddle_x, paddle_y, 100, 30))
 
-    ball = pygame.draw.circle(screen, (BLUE), [650, 325], 15)
+    ball = pygame.draw.circle(screen, (BLUE), [ballx, bally], 15)
+
+    if paddle.colliderect(ball):
+        ballmove_y = ballmove_y * -1
+        ballx = ballx + 10
+
+    ballx = ballmove_x + ballx
+    bally = ballmove_y + bally
+    if ballx < screen_left:
+        ballmove_x = ballmove_x * -1
+
+    if ballx > screen_right:
+        ballmove_x = ballmove_x * -1
+
+    if bally < screen_top:
+        ballmove_y = ballmove_y * -1
+
+    if bally > screen_bottom:
+        ballx = 650
+        bally = 325 
+
+    
+        
+
 
 #add ball movement, and walls
 
